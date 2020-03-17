add6f752b785d4ae349acc499e5b7d25b2fe863c
calivan0423 <kogr2378@naver.com>
2020-03-17 15:02:08 +0900

    20200317

---
+++ asd/main.go
@@ -0,0 +1,41 @@
+package main
+
+//calivan0423
+
+import (
+	"fmt"
+
+	"github.com/calivan0423/dictionary/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{"first": "first word", "second": "second word"}
+
+	err := dictionary.Add("hello", "Greeting")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	definition, err2 := dictionary.Search("hello")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	fmt.Println(definition)
+
+	baseWord := "bye"
+	dictionary.Add(baseWord, "Fisrt")
+	err3 := dictionary.Update(baseWord, "Second")
+	if err3 != nil {
+		fmt.Println(err3)
+	}
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
+
+	dictionary.Delete(baseWord)
+	word2, err4 := dictionary.Search(baseWord)
+	if err4 != nil {
+		fmt.Println(err4)
+	}
+	fmt.Println(word2)
+
+}
